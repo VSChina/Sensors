03a5b1363a2943d7c79628d4fa894fcf58f783de
Yiri Wen <yirwen@microsoft.com>
2017-04-13 16:24:58 +0800

    enable lis3mdl

--- src/lis2mdl_class.h
+++ src/lis2mdl_class.h
@@ -1,10 +1,10 @@
 /**
  ******************************************************************************
- * @file    lis3mdl_class.h
+ * @file    lis2mdl_class.h
  * @author  AST / EST
  * @version V0.0.1
  * @date    14-April-2015
- * @brief   Header file for component LIS3MDL
+ * @brief   Header file for component LIS2MDL
  ******************************************************************************
  * @attention
  *
@@ -35,84 +35,67 @@
  ******************************************************************************
 */
 
-#ifndef __LIS3MDL_CLASS_H
-#define __LIS3MDL_CLASS_H
+#ifndef __LIS2MDL_CLASS_H
+#define __LIS2MDL_CLASS_H
 
 /* Includes ------------------------------------------------------------------*/
 #include "mbed.h"
 #include "ST_INTERFACES\DevI2C.h"
-#include "utility\lis3mdl.h"
+#include "utility\lis2mdl.h"
 #include "ST_INTERFACES\MagneticSensor.h"
 
 /* Classes -------------------------------------------------------------------*/
-/** Class representing a LIS3MDL sensor component
+/** Class representing a LIS2MDL sensor component
  */
-class LIS3MDL : public MagneticSensor {
+class LIS2MDL : public MagneticSensor {
  public:
 	/** Constructor
 	 * @param[in] i2c device I2C to be used for communication
 	 */
-        LIS3MDL(DevI2C &i2c) : MagneticSensor(), dev_i2c(i2c) {
+        LIS2MDL(DevI2C &i2c) : MagneticSensor(), dev_i2c(i2c) {
 	}
 	
 	/** Destructor
 	 */
-        virtual ~LIS3MDL() {}
+        virtual ~LIS2MDL() {}
 	
 	/*** Interface Methods ***/
-	virtual int init(void *init_struct) {
-		return LIS3MDL_Init((MAGNETO_InitTypeDef*)init_struct);
+	virtual int init(void *) {
+		return LIS2MDL_Init();
 	}
 
 	virtual int read_id(uint8_t *m_id) {
-		return LIS3MDL_Read_M_ID(m_id);
+		return LIS2MDL_Read_M_ID(m_id);
 	}
 
 	virtual int get_m_axes(int32_t *pData) {
-		return LIS3MDL_M_GetAxes(pData);
+		return LIS2MDL_M_GetAxes(pData);
 	}
 
 	virtual int get_m_axes_raw(int16_t *pData) {
-		return LIS3MDL_M_GetAxesRaw(pData);
+		return LIS2MDL_M_GetAxesRaw(pData);
 	}
 
  protected:
 	/*** Methods ***/
-	MAGNETO_StatusTypeDef LIS3MDL_Init(MAGNETO_InitTypeDef *LIS3MDL_Init);
-	MAGNETO_StatusTypeDef LIS3MDL_Read_M_ID(uint8_t *m_id);
-	MAGNETO_StatusTypeDef LIS3MDL_M_GetAxes(int32_t *pData);
-	MAGNETO_StatusTypeDef LIS3MDL_M_GetAxesRaw(int16_t *pData);
+	MAGNETO_StatusTypeDef LIS2MDL_Init();
+	MAGNETO_StatusTypeDef LIS2MDL_Read_M_ID(uint8_t *m_id);
+	MAGNETO_StatusTypeDef LIS2MDL_M_GetAxes(int32_t *pData);
+	MAGNETO_StatusTypeDef LIS2MDL_M_GetAxesRaw(int16_t *pData);
 
 	/**
-	 * @brief  Configures LIS3MDL interrupt lines for NUCLEO boards
-	 */
-	void LIS3MDL_IO_ITConfig(void)
-	{
-		/* To be implemented */
-	}
-
-	/**
-	 * @brief  Configures LIS3MDL I2C interface
-	 * @return MAGNETO_OK in case of success, an error code otherwise
-	 */
-	MAGNETO_StatusTypeDef LIS3MDL_IO_Init(void)
-	{
-		return MAGNETO_OK; /* done in constructor */
-	}
-
-	/**
-	 * @brief      Utility function to read data from LIS3MDL
+	 * @brief      Utility function to read data from LIS2MDL
 	 * @param[out] pBuffer pointer to the byte-array to read data in to
 	 * @param[in]  RegisterAddr specifies internal address register to read from.
 	 * @param[in]  NumByteToRead number of bytes to be read.
 	 * @retval     MAGNETO_OK if ok, 
 	 * @retval     MAGNETO_ERROR if an I2C error has occured
 	 */
-	MAGNETO_StatusTypeDef LIS3MDL_IO_Read(uint8_t* pBuffer, 
+	MAGNETO_StatusTypeDef LIS2MDL_IO_Read(uint8_t* pBuffer, 
 					      uint8_t RegisterAddr, uint16_t NumByteToRead)
 	{
 		int ret = dev_i2c.i2c_read(pBuffer,
-					   LIS3MDL_M_MEMS_ADDRESS,
+					   LIS2MDL_M_MEMS_ADDRESS,
 					   RegisterAddr,
 					   NumByteToRead);
 		if(ret != 0) {
@@ -122,18 +105,18 @@ class LIS3MDL : public MagneticSensor {
 	}
 	
 	/**
-	 * @brief      Utility function to write data to LIS3MDL
+	 * @brief      Utility function to write data to LIS2MDL
 	 * @param[in]  pBuffer pointer to the byte-array data to send
 	 * @param[in]  RegisterAddr specifies internal address register to read from.
 	 * @param[in]  NumByteToWrite number of bytes to write.
 	 * @retval     MAGNETO_OK if ok, 
 	 * @retval     MAGNETO_ERROR if an I2C error has occured
 	 */
-	MAGNETO_StatusTypeDef LIS3MDL_IO_Write(uint8_t* pBuffer, 
+	MAGNETO_StatusTypeDef LIS2MDL_IO_Write(uint8_t* pBuffer, 
 					       uint8_t RegisterAddr, uint16_t NumByteToWrite)
 	{
 		int ret = dev_i2c.i2c_write(pBuffer,
-					    LIS3MDL_M_MEMS_ADDRESS,
+					    LIS2MDL_M_MEMS_ADDRESS,
 					    RegisterAddr,
 					    NumByteToWrite);
 		if(ret != 0) {
@@ -147,4 +130,4 @@ class LIS3MDL : public MagneticSensor {
 	DevI2C &dev_i2c;
 };
 
-#endif // __LIS3MDL_CLASS_H
+#endif // __LIS2MDL_CLASS_H
